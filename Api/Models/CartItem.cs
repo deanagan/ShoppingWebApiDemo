326a21c0deeb68bb68452ded7a5aca05796efa1b
Dean Agan <agandfr@gmail.com>
2020-03-05 13:58:27 +1100

    Removed cart id from cart item

--- Api/Models/CartItem.cs
+++ Api/Models/CartItem.cs
@@ -7,14 +7,12 @@ namespace Api.Models
         [Key]
         public int Id { get; }
         public int ProductId { get; }
-        public int CartId { get; }
 
         public int Quantity { get; }
 
         public CartItem(int productId, int cartId, int quantity)
         {
             this.ProductId = productId;
-            this.CartId = cartId;
             this.Quantity = quantity;
         }
     }
