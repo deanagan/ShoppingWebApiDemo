1687562535f7928bd8613eeba997e2468562f24f
Dean Agan <agandfr@gmail.com>
2020-03-05 14:08:11 +1100

    Create cart model. For now, only cart item ids and price rule

---
+++ Api/Models/Cart.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace Api.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int Id { get; }
+        public List<int> CartItemIds { get; }
+
+        public PriceRule PriceRule { get; set; }
+
+    }
+}
