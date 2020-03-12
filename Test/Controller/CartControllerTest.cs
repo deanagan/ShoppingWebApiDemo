59352101e5817e1d877f004a83f6a26e7db75963
Dean Agan <agandfr@gmail.com>
2020-03-12 22:28:02 +1100

    Add test for removal

--- Test/Controller/CartControllerTest.cs
+++ Test/Controller/CartControllerTest.cs
@@ -16,7 +16,7 @@ namespace Test.Controller
     {
 
         [Fact]
-        public void AddCartItemRetusOK_WhenAddingCartItem()
+        public void AddCartItemReturnsOK_WhenAddingCartItem()
         {
             // Arrange
             var cartService = Mock.Of<ICartService>();
@@ -29,6 +29,22 @@ namespace Test.Controller
             response.Should().NotBeNull().And.BeOfType<OkResult>();
         }
 
+        [Fact]
+        public void RemoveCartItemReturnsSuccess_WhenRemovingSameCartItemsMultipleTimes()
+        {
+            // Arrange
+            var cartService = Mock.Of<ICartService>();
+            var controller = new CartController(cartService);
+            Mock.Get(cartService).Setup(cs => cs.RemoveProduct(It.IsAny<string>())).Returns(true);
+
+            // Act
+            var response = controller.RemoveProduct("PROD_001");
+            response = controller.RemoveProduct("PROD_001");
+
+            // Assert
+            // Because delete is an idempotent command, multiple deletions should always be 200/204.
+            response.Should().NotBeNull().And.BeOfType<OkResult>();
+        }
      
     }
 }
