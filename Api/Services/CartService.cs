cf620598dccd1a811076dced495676d3da48fa87
Dean Agan <agandfr@gmail.com>
2020-03-15 20:10:53 +1100

    Re implement cart service to match new interface

--- Api/Services/CartService.cs
+++ Api/Services/CartService.cs
@@ -8,36 +8,28 @@ namespace Api.Services
 {
     public class CartService : ICartService
     {
-        private IProductRepository productRepository;
         private ICartItemRepository cartItemRepository;
 
         public CartService(IProductRepository productRepository, ICartItemRepository cartItemRepository)
         {
-            this.productRepository = productRepository;
             this.cartItemRepository = cartItemRepository;
         }
 
-        public void AddProduct(Product product)
+        public void AddItem(CartItem cartItem)
         {
-            if (product != null)
+            if (cartItem != null)
             {
-                var cartItem = new CartItem { ProductId = product.Id, Quantity = 1 };
                 cartItemRepository.AddCartItem(cartItem);
-            }  
-        }
-
-        public bool RemoveProduct(int productId)
-        {
-            var product = productRepository.GetProducts()
-                                    .Where(product => product.Id == productId)                                        
-                                    .DefaultIfEmpty(null)
-                                    .First();
-            if (product == null)
-            {
-                return false;
             }
+        }
+        
+        public bool RemoveItem(int cartItemId)
+        {
             var cartItems = cartItemRepository.GetCartItems();
-            var cartItemToRemove = cartItems.Where(ci => ci.ProductId == product.Id).First();
+            var cartItemToRemove = cartItems.Where(cartItem => cartItem.Id == cartItemId)
+                                            .Select(c=>c)
+                                            .DefaultIfEmpty(null)
+                                            .First();
             return cartItemRepository.RemoveCartItem(cartItemToRemove);
         }
 
